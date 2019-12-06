5ed39bef4fd24186de1139fd5fd9ae7dbba2aac5
agustina <agustinapereziriarte@gmail.com>
2019-12-06 11:28:11 +0100

    added new user model

--- app/core/models.py
+++ app/core/models.py
@@ -1,11 +1,29 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.core.validators import validate_email
+from django.core.exceptions import ValidationError
 
 
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
+        try:
+            email = self.normalize_email(email)
+            validate_email(email)
+        except ValidationError as e:
+            raise ValidationError(e)
+
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        user = self.create_user(email, password)
+
+        user.is_superuser = True
+        user.is_staff = True
+
         user.save(using=self._db)
 
         return user
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -5,7 +5,7 @@ from django.contrib.auth import get_user_model
 class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
-        email = "mea@gmail.com"
+        email = "mea2@gmail.com"
         password = "lkfbdlfldjfglkf"
 
         user = get_user_model().objects.create_user(
@@ -15,3 +15,36 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        email = "mea2@gmaIl.cOm "
+        normalized_email = "mea2@gmail.com"
+
+        user = get_user_model().objects.create_user(
+            email=email,
+            password="randompassword"
+        )
+
+        self.assertEqual(user.email, normalized_email)
+
+    def test_new_user_invalid_email(self):
+        from django.core.exceptions import ValidationError
+
+        invalid_emails = ["mea2@gmaIlcOm ", "mea2gmail.com", " "]
+
+        for email in invalid_emails:
+            with self.assertRaises(ValidationError):
+                user = get_user_model().objects.create_user(
+                    email=email,
+                    password="randompassword"
+                )
+
+    def test_create_new_superuser(self):
+        user = get_user_model().objects.create_superuser(
+            email="mea2@gmail.com",
+            password="randompassword"
+        )
+
+        self.assertIsNotNone(user)
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
