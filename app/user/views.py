72aafbd88080abbe901dcfca875cc0849364b6ca
agustina <agustinapereziriarte@gmail.com>
2019-12-11 21:40:32 +0100

    added manageuser endpoint

--- app/user/views.py
+++ app/user/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, authentication, permissions
 # from django.shortcuts import render
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import APISettings, api_settings
@@ -12,3 +12,13 @@ class CreateUserView(generics.CreateAPIView):
 class CreateTokenView(ObtainAuthToken):
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authenticated user"""
+    serializer_class = UserSerializer
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user
